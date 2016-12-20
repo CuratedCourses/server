993a989bc37653f370913755c8effd9dfc136af1
Jim Fowler <fowler@math.osu.edu>
2016-12-20 09:11:57 -0500

    deployment script for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name        : "app",
+    script      : "./app.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+       "NODE_ENV": "production"
+    }
+  }]
+}
