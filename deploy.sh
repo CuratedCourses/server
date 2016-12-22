f7ce6c9ecd390c42eabc8b9dc0ff1c5e5bd48aaf
Jim Fowler <fowler@math.osu.edu>
2016-12-22 11:53:37 -0500

    Slightly nicer deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-git pull && npm install && bower install && gulp build && pm2 start ecosystem.config.js --env production
+#! /bin/bash
+pushd "$(dirname "$(readlink "$BASH_SOURCE" || echo "$BASH_SOURCE")")"
+git pull && npm install && bower install && gulp build && pm2 stop all && pm2 start ecosystem.config.js --env production
+popd
