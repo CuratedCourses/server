f60d2cb3abce6a3213e34840a4f647b59e13420f
Drew Youngren <dcy2@columbia.edu>
2018-08-08 15:56:06 -0500

    Try David's meta stuff.

--- controllers/meta.js
+++ controllers/meta.js
@@ -4,26 +4,9 @@
  * Meta Controller
  */
 
-module.exports.controller = function (app) {
-
-  /**
-   * GET /meta/fcla-topics
-   * David's topic's graph for Rob's book.
-   */
-
-  app.get('/meta/fcla-topics-graph', function (req, res) {
-    res.render('/meta/fcla-topics-graph', {
-      url: req.url
-    });
-  });
-
-  /**
-   * GET /guides/instructor-guide
-   */
-
-  app.get('/guides/instructor-guide', function (req, res) {
-    res.render('guides//instructor-guide', {
-	url: req.url,
+module.exports.controller = function(app) {
+  app.get('/meta', function(req, res) {  // When user requests meta page
+    res.render('meta/meta', {           // Render meta page
     });
   });
 };
